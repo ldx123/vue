da285f3d46939a63dfc55839332beed6efb3109a
ldx123 <981280506@qq.com>
2017-04-09 15:09:17 +0800

    1 commit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+	<div>
+		<transition name="router-fade" mode="out-in">
+    		<router-view></router-view>
+    	</transition>
+    </div>
 </template>
-<style lang="sass">
-  @import '../static/style/reset.scss';
-  @import '../static/style/common.scss';
+
+<script>
+
+  	export default {
+    	
+  	}
+
+</script>
+
+<style lang="scss">
+  	@import './style/common';
+	.router-fade-enter-active, .router-fade-leave-active {
+	  	transition: opacity .3s;
+	}
+	.router-fade-enter, .router-fade-leave-active {
+	  	opacity: 0;
+	}
 </style>
